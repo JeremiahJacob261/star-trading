ea9963db40d855de6e5f7e3ebb7cf58514715f4e
Jeremiah <jeremiahjacob261@gmail.com>
2024-06-03 08:30:32 +0100

    design 1.4

---
+++ pages/stocks.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import {Link} from 'react-router-dom';
+import Headc from '../img/touch.png'
+import HeaderDraw from '../components/HeaderDraw';
+import Ready from '../components/ready';
+export default function Stocks() {
+    return(
+            <HeaderDraw>
+            <div className="App"
+        style={{
+          backgroundImage: `url(${Headc})`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat"
+        }}>
+                <div className='stocker'>
+                <p className='first-title'>Unrivalled Opportunities</p>
+                <p className='first-p'>To Trade the Top-performing Stocks in the Global Stock Markets.</p>
+                <div className="btn-case">
+              <Link to="/register" style={{ textDecoration:'none',color:'white'}}>
+                <motion.div className="btn-dark" whileHover={{ y: -10 }} whileTap={{ scale: 0.8 }}>Open Account</motion.div>
+                </Link>
+
+              <Link to="/register" style={{ textDecoration:'none',color:'white'}}>
+                <motion.div className="btn-light" whileHover={{ y: -10 }} whileTap={{ scale: 0.8 }}>Start Trading</motion.div>
+                </Link>
+                </div>
+                </div>
+            </div>
+                </HeaderDraw>
+    )
+}
---
+++ pages/vipbenefits.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import HeaderDraw from '../components/HeaderDraw';
+export default function VIP() {
+    return(
+            <HeaderDraw>
+                <p className='title'>All your crypto needs under one roof</p>
+
+                </HeaderDraw>
+    )
+}
