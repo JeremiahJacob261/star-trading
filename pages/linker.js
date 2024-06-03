ad07f41ca2bf30123e0b77b35e3335a0b3f15a7d
Jeremiah <jeremiahjacob261@gmail.com>
2024-06-03 09:25:54 +0100

    fix 2

--- pages/linker.js
+++ pages/linker.js
@@ -1,7 +1,8 @@
 import Link from 'next/link'
 function Linker({children} ){
+  const child = children ?? " ";
     return(
-      <Link className="linkers" style={{color:'white'}} href={'/'+children.toString().replace(" ",'').toLowerCase()}>
+      <Link className="linkers" style={{color:'white'}} href={'/'+child.replace(" ",'').toLowerCase()}>
         {children}
         </Link>
     )
